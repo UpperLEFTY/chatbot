85dcc1a5d88c5b38f9c97e7b4b2f5600b90c01e6
UpperLEFTY <upperlefty@zoho.eu>
2018-04-17 23:05:24 +0200

    index.js

--- index.js
+++ index.js
@@ -10,5 +10,23 @@ app.set('port', (process.env.PORT || 5000))
 
 // Allows me to process the data
 app.use(bodyParser.urlencoded({extended: false}))
-
 app.use(bodyParser.json())
+
+// ROUTES
+
+app.get('/', function(req, res) {
+    res.send("Hi I am a chatbot")
+  })
+
+  // Facebook
+
+  app.get('/webhook/',function(req, res) {
+    if(req.query['hub.verify_token'] === "oqHhAsdX!&9!y9$") {
+       res.send(req.query['hub.challenge'])
+    }
+    res.send("Wrong token")
+  })
+
+  app.listen(app.get('port'), function() {
+      console.log("running: port")
+    })
