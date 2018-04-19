2561e0fb6df077ed89c8efc726232046bb6c5e9d
UpperLEFTY <upperlefty@zoho.eu>
2018-04-19 16:54:18 +0200

    Adding new Chatbot libraries

--- index.js
+++ index.js
@@ -36,7 +36,8 @@ app.get('/', function(req, res) {
           let sender = event.sender.id
           if (event.message && event.message.text) {
               let text = event.message.text
-              sendText(sender, "Text echo: " + text.substring(0, 100))
+              decideMessage(send)
+              //sendText(sender, "Text echo: " + text.substring(0, 100))
           }
       }
       res.sendStatus(200)
